ae5dad2924e8447cded4e0cf1147587eac597285
hayatokurokawa <hayato.kurokawa@nextam.jp>
2024-11-15 09:47:39 +0900

    予約画面のデイリースケジュール部分の追加モーダルレイアウト修正

--- src/main/resources/static/js/component/individualReservationModal.js
+++ src/main/resources/static/js/component/individualReservationModal.js
@@ -14,15 +14,7 @@ Vue.component('individual-reservation-modal', {
 	            <!-- 入力エリア -->
 	            <div class="modal-content">
 	            	<div class="row">
-	            		<div class="col-12">
-	                    	<select class="individualSelect" style="width: 300px;">
-	                    		<option></option>
-	                    		<option>山田太郎</option>
-							</select>
-	                	</div>
-	            	</div>
-	            	<div class="row">
-	            		<div class="col-10" style="margin-top: 30px; font-size: 20px;">
+	            		<div class="col-10" style="margin-top: 5px; font-size: 20px;">
 	                    	<label class="modal-hedder-title">測定時間</label>
 	                	</div>
 	                	<div class="col-6" style="font-size: 30px;">
@@ -32,20 +24,87 @@ Vue.component('individual-reservation-modal', {
 	                    	<label class="input-label"><input type="checkbox"><span class="spaceLeft">30分</span></label>
 	                	</div>
 	            	</div>
+	            	<div class="row" style="margin-top:10px;">
+			            		<div class="col-4">
+									<label　class="item-title">カナ</label>
+			            		</div>
+			            		<div class="col-8">
+			            			<input type="text" class="individual-text">
+			            		</div>
+			            	</div>
+	            	<div class="row"  style="margin-top:10px;">
+						<div class="col-6" style="min-height:40px;">
+							<button class="modal-regist" style="width: 90px;">検索</button>
+						</div>
+						<div class="col-6" style="min-height:40px;">
+							<button class="modal-regist" style="width: 90px;">条件クリア</button>
+						</div>
+	            	</div>
+	            	<div v-for="(item, index) in accordionItems" :key="index" class="accordion-item" style="margin-top:10px;">
+					<div class="accordion-header" @click="toggleAccordion(index)">
+						<label style="margin-top: 15px;">{{ item.title }}</label>
+					</div>
+					<div v-if="item.open" class="accordion-content">
+							<div class="row">
+			            		<div class="col-4">
+									<label　class="item-title">カナ</label><label class="label-required">必須</label>
+			            		</div>
+			            		<div class="col-8">
+			            			<input type="text" class="individual-text">
+			            		</div>
+			            	</div>
+			            	<div class="row">
+			            		<div class="col-4">
+									<label　class="item-title">メールアドレス</label><label class="label-required">必須</label>
+			            		</div>
+			            		<div class="col-8">
+			            			<input type="text" class="individual-text">
+			            		</div>
+			            	</div>
+			            	<div class="row">
+			            		<div class="col-4">
+									<label　class="item-title">電話番号</label><label class="label-required">必須</label>
+			            		</div>
+			            		<div class="col-8">
+			            			<input type="text" class="individual-text">
+			            		</div>
+			            	</div>
+			            	<div class="row">
+			            		<div class="col-12 botton-area" style="min-height:40px; margin:10px 0px; text-align:center;">
+									<button class="modal-regist">新規登録</button>
+								</div>
+			            	</div>
+						</div>
+					</div>
+	            	<div class="row" :class="{ 'selected-row': selectedRow === 1 }" @click="selectRow(1)" style="margin-top: 20px; font-size: 15px;border: 1px solid; padding: 10px;">
+					    <div class="col-4">
+					        <label>山田太郎</label>
+					    </div>
+					    <div class="col-8">
+					        <label>千葉県銚子市若宮町102-9</label>
+					    </div>
+					</div>
+					<div class="row" :class="{ 'selected-row': selectedRow === 2 }" @click="selectRow(2)" style="margin-top: 5px; font-size: 15px;border: 1px solid; padding: 10px;">
+					    <div class="col-4">
+					        <label>山田隆</label>
+					    </div>
+					    <div class="col-8">
+					        <label>青森県平川市館田稲村959-8</label>
+					    </div>
+					</div>
+					<div class="row" :class="{ 'selected-row': selectedRow === 3 }" @click="selectRow(3)" style="margin-top: 5px; font-size: 15px;border: 1px solid; padding: 10px;">
+					    <div class="col-4">
+					        <label>山田隆介</label>
+					    </div>
+					    <div class="col-8">
+					        <label>北海道北斗市中央4-837-13</label>
+					    </div>
+					</div>
 	            </div><!-- /modal-content -->
                 <div class="row">
-	                <div class="col-6">
-	                    <div class="row">
-	                        <div class="col-12 botton-area" style="min-height:40px;">
-	                            <button class="modal-delete">削除</button>
-	                        </div>
-	                    </div>
-	                </div>
-	                <div class="col-6">
-	                    <div class="row">
-	                        <div class="col-12 botton-area" style="min-height:40px;">
-	                            <button class="modal-regist">登録</button>
-	                        </div>
+	                <div class="col-12">
+	                    <div class="col-12 botton-area" style="min-height:40px;">
+	                    	<button class="modal-regist">登録</button>
 	                    </div>
 	                </div>
                 </div>
@@ -58,6 +117,10 @@ Vue.component('individual-reservation-modal', {
 			displayFlg: false,
 			// 進捗フラグ
 			processingFlg:false,
+			selectedRow: null,
+			accordionItems: [
+                { title: '新規お客様登録', open: false, inputValue: '' },
+            ],
     	}
     },
     computed:{
@@ -80,5 +143,12 @@ Vue.component('individual-reservation-modal', {
 		closeModalProcessing: function() {
 			this.processingFlg = false;
 		},
+		selectRow(rowNumber) {
+	        this.selectedRow = rowNumber;
+	    },
+	    toggleAccordion: function(index) {
+            // アコーディオンの開閉
+            this.accordionItems[index].open = !this.accordionItems[index].open;
+        },
     },
 });
